51827f57f6b3aff70ac23136c0451e10b4a811bf
kishore <kishore01.official@gmail.com>
2023-09-22 11:02:46 +0530

    Array Functions completed

--- Array.js
+++ Array.js
@@ -6,16 +6,37 @@ const port = 5000;
 const app = express();
 
 app.get("/", (req, res) => {
-    const arr = [1, 2, 3, 4, 5, 6];
+    let arr = [1, 2, 3, 4, 5, 6];
     //chunk
     const out = _.chunk(arr, 4);
-    console.log(out);
+    console.log("chunk--", out);
 
     //findIndex
     const pos = _.findIndex(arr, (num) => num == 4);
-    console.log(pos, " : ", arr);
-    res.send(`Hello : ${out}`);
-})
+    console.log("findIndex--", pos, " : ", arr);
+
+    //flattern
+    arr = [1, [2, 3], [4], 5];
+    console.log("flattern--", _.flatten(arr));
+
+    //indexOf
+    arr = [1, 2, 1, 2];
+    console.log("indexOf--", arr, _.indexOf(arr, 2));
+    console.log(_.indexOf(arr, 2, 2));
+
+    //join
+    arr = ['a', 'b', 'c'];
+    console.log("join--", _.join(arr, '-'));
+
+    //slice
+    console.log("slice--", _.slice(arr));
+
+    //uniq
+    arr = [1, 2, 3, 2, 1];
+    console.log("uniq--", _.uniq(arr));
+
+    res.send(`Hello : ${arr}`);
+});
 
 app.listen(port, () => {
     console.log("The server is running");
